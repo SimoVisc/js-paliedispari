d466468a9d6a9623119f8250aea62ea61621abac
SimoVisc <simonaviscardivs@gmail.com>
2022-10-28 15:57:10 +0200

    Aggiunge alert per dichiarare se la  parola inserita è palindroma

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,9 +1,18 @@
 "use strict"
 // Chiedere all’utente di inserire una parola
-const userWord=(prompt("unserisci una parola"))
+let userWord=(prompt("inserisci una parola"));
 // Creare una funzione per capire se la parola inserita è palindroma
+let palindroma= ""
 for(let i = userWord.length -1; i >= 0; i--){
     console.log(userWord[i]);
+    palindroma += userWord[i]
+    if(palindroma === userWord) {
+        alert("Questa parola è palindroma")
+    }
 }
+if(palindroma !== userWord) {
+    alert("Questa parola non è palindroma")
+}
+
 
 
