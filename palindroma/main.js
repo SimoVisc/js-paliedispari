f7ec743a98e967a08073dbb5cf6c4d5ae618e82a
SimoVisc <simonaviscardivs@gmail.com>
2022-10-28 15:29:39 +0200

    Aggiunge il comando per creare una parola al contrario

---
+++ palindroma/main.js
@@ -0,0 +1,9 @@
+"use strict"
+// Chiedere all’utente di inserire una parola
+const userWord=(prompt("unserisci una parola"))
+// Creare una funzione per capire se la parola inserita è palindroma
+for(let i = userWord.length -1; i >= 0; i--){
+    console.log(userWord[i]);
+}
+
+
